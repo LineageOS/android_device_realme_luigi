93311f3f09a4b34947df9c35a914e9521fb924a4
Bruno Martins <bgcngm@gmail.com>
2024-09-07 22:47:03 +0100

    luigi: Patch libAlgoProcess to resolve duplicate dependencies
    Fixes:
    error: module "libAlgoProcess" variant
    "android_vendor_arm64_armv9-a_static": depends on multiple
    versions of the same aidl_interface:
    android.hardware.graphics.common-V1-ndk-source,
    android.hardware.graphics.common-V5-ndk-source
    
    Change-Id: If0be3da5c13e45c6c59dde98bb80f35eb22bf846

--- extract-files.sh
+++ extract-files.sh
@@ -5,6 +5,24 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function blob_fixup() {
+    case "${1}" in
+        odm/lib64/libAlgoProcess.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --replace-needed "android.hardware.graphics.common-V1-ndk_platform.so" "android.hardware.graphics.common-V5-ndk.so" "${2}"
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+
+    return 0
+}
+
+function blob_fixup_dry() {
+    blob_fixup "$1" ""
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
