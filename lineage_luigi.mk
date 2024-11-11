7551ebdde51415d1f9ce6412bf0d72d462b4131f
Vivekachooz <vivekachooz@gmail.com>
2024-11-11 20:55:56 +0530

    luigi: Update from RMX3660_14.0.0.1100(EX01)
    Change-Id: I2e1fdd0d5f29386a8583e2e54ce81975b3e575d5

--- lineage_luigi.mk
+++ lineage_luigi.mk
@@ -24,8 +24,8 @@ PRODUCT_MANUFACTURER := realme
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="RE588BL1-user 14 UKQ1.230924.001 T.R4T2.1927be9-3bb5_697a2 release-keys" \
-    BuildFingerprint=realme/RMX3471/RE54CBL1:14/UKQ1.230924.001/S.194c139-3700-36fe:user/release-keys \
+    BuildDesc="RE588BL1-user 14 UKQ1.230924.001 T.R4T2.1ac2e8e-44f0-131b03 release-keys" \
+    BuildFingerprint=realme/RMX3660/RE588BL1:14/UKQ1.230924.001/T.R4T2.1ac2e8e-44f0-131b03:user/release-keys \
     DeviceName=RE588BL1 \
     DeviceProduct=RE588BL1 \
     SystemDevice=RE588BL1 \
